dd37edc71dd1f4b4d6648b65d52e4569efd1235d
L1mmp <dima.wwe2@yandex.ru>
2021-09-13 00:34:20 +0300

    Добавьте файлы проекта.

---
+++ NM_1/NM_1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <math.h>
+
+double F(double x); //  2.7 * asin(2.22 * x - 1) - x + 1.8 = 0
+
+double ChordMethod(double a, double b, double E);
+double HalfDivisionMethod(double a, double b, double E);
+double SimpleIterationMethod(double a, double b, double E, double q);
+
+using namespace std;
+
+
+int main()
+{
+	cout << ChordMethod(0.0, 0.8, 10e-10) << endl;
+	cout << HalfDivisionMethod(0.0, 1, 10e-10) << endl;
+}
+
+double F(double x)
+{
+	return 2.7 * asin(2.22 * x - 1) - x + 1.8;
+}
+double Fi(double x)
+{
+	return 2.7 * asin(2.22 * x - 1) + 1.8;
+}
+double DifFi(double x)
+{
+	return 5.9994 / (sqrt(2.22 * x - 1));
+}
+
+double ChordMethod(double a, double b, double E)
+{
+	if (F(a) * F(b) > 0)
+	{
+		cout << " Wrong interval!";
+		return 1;
+	}
+
+	double xn = 0.0;
+	while (abs(F(b) - F(a)) > E)
+	{
+
+		xn = a - (F(a) / (F(b) - F(a))) * (b - a);
+		if (F(a) * F(xn) < 0) b = xn;
+		else a = xn;
+	}
+	return xn;
+}
+
+double HalfDivisionMethod(double a, double b, double E)
+{
+	if (F(a) * F(b) > 0)
+	{
+		cout << " Wrong interval!" << endl;
+	}
+
+	double xn = 0.0;
+	while ((b - a > E))
+	{
+		xn = (a + b) / 2;
+		if (F(a) * F(xn) < 0) b = xn;
+		else a = xn;
+	}
+	return xn;
+}
+
+double SimpleIterationMethod(double x, double a, double E, double q)
+{
+	double xn;
+	while (abs(xn) <= (1 - q) * E / q)
+	{
+		// TODO : Завтра доделать, если будет на лекции
+	}
+}
+
