fc6adf3863431455a4d974b68bf71a970a23112a
L1mmp <dima.wwe2@yandex.ru>
2021-09-13 20:24:26 +0300

    qwe

--- NM_1/NM_1.cpp
+++ NM_1/NM_1.cpp
@@ -5,7 +5,7 @@ double F(double x); //  2.7 * asin(2.22 * x - 1) - x + 1.8 = 0
 
 double ChordMethod(double a, double b, double E);
 double HalfDivisionMethod(double a, double b, double E);
-double SimpleIterationMethod(double a, double b, double E, double q);
+double SimpleIterationMethod(double x0, double E);
 
 using namespace std;
 
@@ -14,19 +14,20 @@ int main()
 {
 	cout << ChordMethod(0.0, 0.8, 10e-10) << endl;
 	cout << HalfDivisionMethod(0.0, 1, 10e-10) << endl;
+	cout << SimpleIterationMethod(0.3, 10e-10) << endl;
 }
 
 double F(double x)
 {
 	return 2.7 * asin(2.22 * x - 1) - x + 1.8;
 }
-double Fi(double x)
+double Phi(double x)
 {
 	return 2.7 * asin(2.22 * x - 1) + 1.8;
 }
-double DifFi(double x)
+double DifPhi(double x)
 {
-	return 5.9994 / (sqrt(2.22 * x - 1));
+	return 5.9994 / (sqrt(4.9284 * pow(x, 2) - 2.22 * x));
 }
 
 double ChordMethod(double a, double b, double E)
@@ -40,7 +41,6 @@ double ChordMethod(double a, double b, double E)
 	double xn = 0.0;
 	while (abs(F(b) - F(a)) > E)
 	{
-
 		xn = a - (F(a) / (F(b) - F(a))) * (b - a);
 		if (F(a) * F(xn) < 0) b = xn;
 		else a = xn;
@@ -65,12 +65,20 @@ double HalfDivisionMethod(double a, double b, double E)
 	return xn;
 }
 
-double SimpleIterationMethod(double x, double a, double E, double q)
+double SimpleIterationMethod(double x0, double E)
 {
-	double xn;
-	while (abs(xn) <= (1 - q) * E / q)
+
+	double xn = Phi(x0);
+	double q;
+	do
 	{
-		// TODO : Завтра доделать, если будет на лекции
-	}
+		cout << "xn: " << xn << " x0: " << x0 << endl;
+		xn = Phi(x0);
+		q = 0.5;
+		cout << "q: " << q << endl;
+		x0 = xn;
+	} while (abs(xn - x0) <= (1 - q) * E / q);
+
+	return xn;
 }
 
